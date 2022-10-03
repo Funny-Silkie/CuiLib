3349c009aa3aea211b8351181ce99388bde315ab
Funny_Silkie <ban.grumble99rar@gmail.com>
2022-10-03 19:09:42 +0900

    implement Helper class

---
+++ src/CuiLib/_Common/IOHelper.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuiLib
+{
+    /// <summary>
+    /// I/O処理のヘルパークラスです。
+    /// </summary>
+    public static class IOHelper
+    {
+        /// <summary>
+        /// UTF-8Nのエンコーディングを取得します。
+        /// </summary>
+        public static Encoding UTF8N { get; } = new UTF8Encoding(false);
+
+        /// <summary>
+        /// ディレクトリが存在しない場合に生成します。
+        /// </summary>
+        /// <param name="directory">ディレクトリのパス</param>
+        /// <returns><paramref name="directory"/>に対応する<see cref="DirectoryInfo"/>のインスタンス</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="directory"/>がnull</exception>
+        /// <exception cref="ArgumentException"><paramref name="directory"/><paramref name="directory"/>が無効</exception>
+        /// <exception cref="System.Security.SecurityException">アクセス権限がない</exception>
+        /// <exception cref="PathTooLongException"><paramref name="directory"/>が長すぎる</exception>
+        /// <exception cref="IOException">ディレクトリの生成に失敗した</exception>
+        public static DirectoryInfo EnsureDirectory(string directory)
+        {
+            var result = new DirectoryInfo(directory);
+            if (!result.Exists) result.Create();
+            return result;
+        }
+
+        /// <summary>
+        /// 全ての文字列を読み取ります。
+        /// </summary>
+        /// <param name="file">使用する<see cref="FileInfo"/>のインスタンス</param>
+        /// <returns>ファイルの全ての文字列</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="file"/>がnull</exception>
+        /// <exception cref="DirectoryNotFoundException"><paramref name="file"/>のディレクトリが存在しない</exception>
+        /// <exception cref="FileNotFoundException"><paramref name="file"/>のファイルが存在しない</exception>
+        /// <exception cref="System.Security.SecurityException">ファイルへのアクセス権限がない</exception>
+        /// <exception cref="UnauthorizedAccessException">ファイルへのアクセスが拒否された</exception>
+        /// <exception cref="OutOfMemoryException">メモリが足りない</exception>
+        /// <exception cref="IOException">I/Oエラーが発生した</exception>
+        public static string OpenReadAllText(this FileInfo file)
+        {
+            ArgumentNullException.ThrowIfNull(file);
+
+            using StreamReader reader = file.OpenText();
+            return reader.ReadToEnd();
+        }
+
+        /// <summary>
+        /// 全ての文字列を読み取ります。
+        /// </summary>
+        /// <param name="file">使用する<see cref="FileInfo"/>のインスタンス</param>
+        /// <returns>ファイルの全ての文字列</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="file"/>がnull</exception>
+        /// <exception cref="DirectoryNotFoundException"><paramref name="file"/>のディレクトリが存在しない</exception>
+        /// <exception cref="FileNotFoundException"><paramref name="file"/>のファイルが存在しない</exception>
+        /// <exception cref="System.Security.SecurityException">ファイルへのアクセス権限がない</exception>
+        /// <exception cref="UnauthorizedAccessException">ファイルへのアクセスが拒否された</exception>
+        /// <exception cref="InvalidOperationException">readerが読み取り処理に使用中</exception>
+        /// <exception cref="ArgumentOutOfRangeException">文字列長が<see cref="int.MaxValue"/>を超える</exception>
+        public static async Task<string> OpenReadAllTextAsync(this FileInfo file)
+        {
+            ArgumentNullException.ThrowIfNull(file);
+
+            using StreamReader reader = file.OpenText();
+            return await reader.ReadToEndAsync();
+        }
+
+        /// <summary>
+        /// イテレータとして1行ずつ文字列を読み出します。
+        /// </summary>
+        /// <param name="file">使用する<see cref="FileInfo"/>のインスタンス</param>
+        /// <returns>各行を返すイテレータ</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="file"/>がnull</exception>
+        /// <exception cref="DirectoryNotFoundException"><paramref name="file"/>のディレクトリが存在しない</exception>
+        /// <exception cref="FileNotFoundException"><paramref name="file"/>のファイルが存在しない</exception>
+        /// <exception cref="System.Security.SecurityException">ファイルへのアクセス権限がない</exception>
+        /// <exception cref="UnauthorizedAccessException">ファイルへのアクセスが拒否された</exception>
+        /// <exception cref="OutOfMemoryException">メモリが足りない</exception>
+        /// <exception cref="IOException">I/Oエラーが発生した</exception>
+        public static IEnumerable<string> OpenIterateLines(this FileInfo file)
+        {
+            ArgumentNullException.ThrowIfNull(file);
+
+            using StreamReader reader = file.OpenText();
+            while (!reader.EndOfStream) yield return reader.ReadLine()!;
+        }
+
+        /// <summary>
+        /// イテレータとして1行ずつ文字列を読み出します。
+        /// </summary>
+        /// <param name="file">使用する<see cref="FileInfo"/>のインスタンス</param>
+        /// <returns>各行を返すイテレータ</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="file"/>がnull</exception>
+        /// <exception cref="DirectoryNotFoundException"><paramref name="file"/>のディレクトリが存在しない</exception>
+        /// <exception cref="FileNotFoundException"><paramref name="file"/>のファイルが存在しない</exception>
+        /// <exception cref="System.Security.SecurityException">ファイルへのアクセス権限がない</exception>
+        /// <exception cref="UnauthorizedAccessException">ファイルへのアクセスが拒否された</exception>
+        /// <exception cref="InvalidOperationException">readerが読み取り処理に使用中</exception>
+        /// <exception cref="ArgumentOutOfRangeException">文字列長が<see cref="int.MaxValue"/>を超える</exception>
+        public static async IAsyncEnumerable<string> OpenIterateLinesAsync(this FileInfo file)
+        {
+            ArgumentNullException.ThrowIfNull(file);
+
+            using StreamReader reader = file.OpenText();
+            while (!reader.EndOfStream) yield return (await reader.ReadLineAsync())!;
+        }
+
+        /// <summary>
+        /// 1行ずつ文字列を読み出します。
+        /// </summary>
+        /// <param name="file">使用する<see cref="FileInfo"/>のインスタンス</param>
+        /// <returns>ファイルの各行</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="file"/>がnull</exception>
+        /// <exception cref="DirectoryNotFoundException"><paramref name="file"/>のディレクトリが存在しない</exception>
+        /// <exception cref="FileNotFoundException"><paramref name="file"/>のファイルが存在しない</exception>
+        /// <exception cref="System.Security.SecurityException">ファイルへのアクセス権限がない</exception>
+        /// <exception cref="UnauthorizedAccessException">ファイルへのアクセスが拒否された</exception>
+        /// <exception cref="OutOfMemoryException">メモリが足りない</exception>
+        /// <exception cref="IOException">I/Oエラーが発生した</exception>
+        public static string[] OpenReadLines(this FileInfo file)
+        {
+            return file.OpenIterateLines().ToArray();
+        }
+
+        /// <summary>
+        /// 1行ずつ文字列を読み出します。
+        /// </summary>
+        /// <param name="file">使用する<see cref="FileInfo"/>のインスタンス</param>
+        /// <returns>ファイルの各行</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="file"/>がnull</exception>
+        /// <exception cref="DirectoryNotFoundException"><paramref name="file"/>のディレクトリが存在しない</exception>
+        /// <exception cref="FileNotFoundException"><paramref name="file"/>のファイルが存在しない</exception>
+        /// <exception cref="System.Security.SecurityException">ファイルへのアクセス権限がない</exception>
+        /// <exception cref="UnauthorizedAccessException">ファイルへのアクセスが拒否された</exception>
+        /// <exception cref="InvalidOperationException">readerが読み取り処理に使用中</exception>
+        /// <exception cref="ArgumentOutOfRangeException">文字列長が<see cref="int.MaxValue"/>を超える</exception>
+        public static async Task<string[]> OpenReadLinesAsync(this FileInfo file)
+        {
+            return await file.OpenIterateLinesAsync().ToArrayAsync();
+        }
+
+        /// <summary>
+        /// イテレータとして1行ずつ文字列を読み出します。
+        /// </summary>
+        /// <param name="reader">使用する<see cref="StreamReader"/>のインスタンス</param>
+        /// <returns>各行を返すイテレータ</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="reader"/>がnull</exception>
+        /// <exception cref="ObjectDisposedException"><paramref name="reader"/>が既に破棄されている</exception>
+        /// <exception cref="OutOfMemoryException">メモリが足りない</exception>
+        /// <exception cref="IOException">I/Oエラーが発生した</exception>
+        public static IEnumerable<string> IterateLines(this StreamReader reader)
+        {
+            ArgumentNullException.ThrowIfNull(reader);
+
+            while (!reader.EndOfStream) yield return reader.ReadLine()!;
+        }
+
+        /// <summary>
+        /// イテレータとして1行ずつ文字列を読み出します。
+        /// </summary>
+        /// <param name="reader">使用する<see cref="StreamReader"/>のインスタンス</param>
+        /// <returns>各行を返すイテレータ</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="reader"/>がnull</exception>
+        /// <exception cref="ObjectDisposedException"><paramref name="reader"/>が既に破棄されている</exception>
+        /// <exception cref="IOException">I/Oエラーが発生した</exception>
+        public static async IAsyncEnumerable<string> IterateLinesAsync(this StreamReader reader)
+        {
+            ArgumentNullException.ThrowIfNull(reader);
+
+            while (!reader.EndOfStream) yield return (await reader.ReadLineAsync())!;
+        }
+    }
+}
---
+++ src/CuiLib/_Common/ThrowHelper.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace CuiLib
+{
+    /// <summary>
+    /// 例外処理に関連する実装を行います。
+    /// </summary>
+    public static class ThrowHelper
+    {
+        private static char[]? _invalidPathChars;
+        private static char[]? _invalidFileNameChars;
+
+        /// <summary>
+        /// ファイル名に使用できない文字を取得します。
+        /// </summary>
+        /// <returns>ファイル名に使用できない文字</returns>
+        private static char[] GetInvalidFileNameChars() => _invalidFileNameChars ??= Path.GetInvalidFileNameChars();
+
+        /// <summary>
+        /// パスに使用できない文字を取得します。
+        /// </summary>
+        /// <returns>パスに使用できない文字</returns>
+        private static char[] GetInvalidPathChars() => _invalidPathChars ??= Path.GetInvalidPathChars();
+
+        #region Catch
+
+        /// <summary>
+        /// 例外をキャッチします。
+        /// </summary>
+        /// <param name="action">実行処理</param>
+        /// <param name="error">発生した例外。例外が発生しなかった場合はnull</param>
+        /// <returns>例外が発生せず処理が終了したらtrue，それ以外でfalse</returns>
+        public static bool Catch(Action action, [NotNullWhen(false)] out Exception? error)
+        {
+            ArgumentNullException.ThrowIfNull(action);
+
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception e)
+            {
+                error = e;
+                return false;
+            }
+            error = null;
+            return true;
+        }
+
+        /// <summary>
+        /// 例外をキャッチします。
+        /// </summary>
+        /// <typeparam name="TArg">引数の型</typeparam>
+        /// <param name="action">実行処理</param>
+        /// <param name="arg"><paramref name="action"/>の引数</param>
+        /// <param name="error">発生した例外。例外が発生しなかった場合はnull</param>
+        /// <returns>例外が発生せず処理が終了したらtrue，それ以外でfalse</returns>
+        public static bool Catch<TArg>(Action<TArg?> action, TArg? arg, [NotNullWhen(false)] out Exception? error)
+        {
+            ArgumentNullException.ThrowIfNull(action);
+
+            try
+            {
+                action.Invoke(arg);
+            }
+            catch (Exception e)
+            {
+                error = e;
+                return false;
+            }
+            error = null;
+            return true;
+        }
+
+        /// <summary>
+        /// 例外をキャッチします。
+        /// </summary>
+        /// <typeparam name="TArg1">引数の型</typeparam>
+        /// <typeparam name="TArg2">引数の型</typeparam>
+        /// <param name="action">実行処理</param>
+        /// <param name="arg1"><paramref name="action"/>の引数1</param>
+        /// <param name="arg2"><paramref name="action"/>の引数2</param>
+        /// <param name="error">発生した例外。例外が発生しなかった場合はnull</param>
+        /// <returns>例外が発生せず処理が終了したらtrue，それ以外でfalse</returns>
+        public static bool Catch<TArg1, TArg2>(Action<TArg1?, TArg2?> action, TArg1? arg1, TArg2? arg2, [NotNullWhen(false)] out Exception? error)
+        {
+            ArgumentNullException.ThrowIfNull(action);
+
+            try
+            {
+                action.Invoke(arg1, arg2);
+            }
+            catch (Exception e)
+            {
+                error = e;
+                return false;
+            }
+            error = null;
+            return true;
+        }
+
+        /// <summary>
+        /// 例外をキャッチします。
+        /// </summary>
+        /// <typeparam name="TArg1">引数の型</typeparam>
+        /// <typeparam name="TArg2">引数の型</typeparam>
+        /// <typeparam name="TArg3">引数の型</typeparam>
+        /// <param name="action">実行処理</param>
+        /// <param name="arg1"><paramref name="action"/>の引数1</param>
+        /// <param name="arg2"><paramref name="action"/>の引数2</param>
+        /// <param name="arg3"><paramref name="action"/>の引数3</param>
+        /// <param name="error">発生した例外。例外が発生しなかった場合はnull</param>
+        /// <returns>例外が発生せず処理が終了したらtrue，それ以外でfalse</returns>
+        public static bool Catch<TArg1, TArg2, TArg3>(Action<TArg1?, TArg2?, TArg3?> action, TArg1? arg1, TArg2? arg2, TArg3? arg3, [NotNullWhen(false)] out Exception? error)
+        {
+            ArgumentNullException.ThrowIfNull(action);
+
+            try
+            {
+                action.Invoke(arg1, arg2, arg3);
+            }
+            catch (Exception e)
+            {
+                error = e;
+                return false;
+            }
+            error = null;
+            return true;
+        }
+
+        /// <summary>
+        /// 例外をキャッチします。
+        /// </summary>
+        /// <typeparam name="TResult">生成されるオブジェクトの型</typeparam>
+        /// <param name="factory"><typeparamref name="TResult"/>の生成処理</param>
+        /// <param name="result"><paramref name="factory"/>の生成するオブジェクト。例外発生時は既定値</param>
+        /// <param name="error">発生した例外。例外が発生しなかった場合はnull</param>
+        /// <returns>例外が発生せず処理が終了したらtrue，それ以外でfalse</returns>
+        public static bool Catch<TResult>(Func<TResult?> factory, [MaybeNullWhen(true)] out TResult? result, [NotNullWhen(false)] out Exception? error)
+        {
+            ArgumentNullException.ThrowIfNull(factory);
+
+            try
+            {
+                result = factory.Invoke();
+            }
+            catch (Exception e)
+            {
+                result = default;
+                error = e;
+                return false;
+            }
+            error = null;
+            return true;
+        }
+
+        /// <summary>
+        /// 例外をキャッチします。
+        /// </summary>
+        /// <typeparam name="TArg">引数の型</typeparam>
+        /// <typeparam name="TResult">生成されるオブジェクトの型</typeparam>
+        /// <param name="factory"><typeparamref name="TResult"/>の生成処理</param>
+        /// <param name="arg"><paramref name="factory"/>の引数</param>
+        /// <param name="result"><paramref name="factory"/>の生成するオブジェクト。例外発生時は既定値</param>
+        /// <param name="error">発生した例外。例外が発生しなかった場合はnull</param>
+        /// <returns>例外が発生せず処理が終了したらtrue，それ以外でfalse</returns>
+        public static bool Catch<TArg, TResult>(Func<TArg?, TResult?> factory, TArg? arg, [MaybeNullWhen(true)] out TResult? result, [NotNullWhen(false)] out Exception? error)
+        {
+            ArgumentNullException.ThrowIfNull(factory);
+
+            try
+            {
+                result = factory.Invoke(arg);
+            }
+            catch (Exception e)
+            {
+                result = default;
+                error = e;
+                return false;
+            }
+            error = null;
+            return true;
+        }
+
+        /// <summary>
+        /// 例外をキャッチします。
+        /// </summary>
+        /// <typeparam name="TArg1">引数の型</typeparam>
+        /// <typeparam name="TArg2">引数の型</typeparam>
+        /// <typeparam name="TResult">生成されるオブジェクトの型</typeparam>
+        /// <param name="factory"><typeparamref name="TResult"/>の生成処理</param>
+        /// <param name="arg1"><paramref name="factory"/>の引数1</param>
+        /// <param name="arg2"><paramref name="factory"/>の引数2</param>
+        /// <param name="result"><paramref name="factory"/>の生成するオブジェクト。例外発生時は既定値</param>
+        /// <param name="error">発生した例外。例外が発生しなかった場合はnull</param>
+        /// <returns>例外が発生せず処理が終了したらtrue，それ以外でfalse</returns>
+        public static bool Catch<TArg1, TArg2, TResult>(Func<TArg1?, TArg2?, TResult?> factory, TArg1? arg1, TArg2? arg2, [MaybeNullWhen(true)] out TResult? result, [NotNullWhen(false)] out Exception? error)
+        {
+            ArgumentNullException.ThrowIfNull(factory);
+
+            try
+            {
+                result = factory.Invoke(arg1, arg2);
+            }
+            catch (Exception e)
+            {
+                result = default;
+                error = e;
+                return false;
+            }
+            error = null;
+            return true;
+        }
+
+        /// <summary>
+        /// 例外をキャッチします。
+        /// </summary>
+        /// <typeparam name="TArg1">引数の型</typeparam>
+        /// <typeparam name="TArg2">引数の型</typeparam>
+        /// <typeparam name="TArg3">引数の型</typeparam>
+        /// <typeparam name="TResult">生成されるオブジェクトの型</typeparam>
+        /// <param name="factory"><typeparamref name="TResult"/>の生成処理</param>
+        /// <param name="arg1"><paramref name="factory"/>の引数1</param>
+        /// <param name="arg2"><paramref name="factory"/>の引数2</param>
+        /// <param name="arg3"><paramref name="factory"/>の引数3</param>
+        /// <param name="result"><paramref name="factory"/>の生成するオブジェクト。例外発生時は既定値</param>
+        /// <param name="error">発生した例外。例外が発生しなかった場合はnull</param>
+        /// <returns>例外が発生せず処理が終了したらtrue，それ以外でfalse</returns>
+        public static bool Catch<TArg1, TArg2, TArg3, TResult>(Func<TArg1?, TArg2?, TArg3?, TResult?> factory, TArg1? arg1, TArg2? arg2, TArg3? arg3, [MaybeNullWhen(true)] out TResult? result, [NotNullWhen(false)] out Exception? error)
+        {
+            ArgumentNullException.ThrowIfNull(factory);
+
+            try
+            {
+                result = factory.Invoke(arg1, arg2, arg3);
+            }
+            catch (Exception e)
+            {
+                result = default;
+                error = e;
+                return false;
+            }
+            error = null;
+            return true;
+        }
+
+        #endregion Catch
+
+        #region Throw
+
+        /// <summary>
+        /// 文字列がnullまたか空文字であるかどうかを検証します。
+        /// </summary>
+        /// <param name="value">文字列</param>
+        /// <param name="name">引数名</param>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/>がnull</exception>
+        /// <exception cref="ArgumentException"><paramref name="value"/>が空文字</exception>
+        public static void ThrowIfNullOrEmpty(string? value, [CallerArgumentExpression("value")] string? name = null)
+        {
+            ArgumentNullException.ThrowIfNull(value, name);
+            if (value.Length == 0) throw new ArgumentException("空文字です", name);
+        }
+
+        /// <summary>
+        /// ファイル名に使用できない文字が含まれているかどうかを検証します。
+        /// </summary>
+        /// <param name="fileName">ファイル名またはファイルパス</param>
+        /// <param name="name">引数名</param>
+        /// <exception cref="ArgumentException"><paramref name="fileName"/>に使用できない文字が含まれている</exception>
+        public static void ThrowIfHasInvalidFileNameChar(string fileName, [CallerArgumentExpression("fileName")] string? name = null)
+        {
+            int index = fileName.IndexOfAny(GetInvalidFileNameChars());
+            if (index >= 0) throw new ArgumentException($"ファイル名に使用できない文字'{fileName[index]}'が含まれています", name);
+        }
+
+        /// <summary>
+        /// パスに使用できない文字が含まれているかどうかを検証します。
+        /// </summary>
+        /// <param name="path">パス</param>
+        /// <param name="name">引数名</param>
+        /// <exception cref="ArgumentException"><paramref name="path"/>に使用できない文字が含まれている</exception>
+        public static void ThrowIfHasInvalidPathChar(string path, [CallerArgumentExpression("path")] string? name = null)
+        {
+            int index = path.IndexOfAny(GetInvalidPathChars());
+            if (index >= 0) throw new ArgumentException($"パスに使用できない文字'{path[index]}'が含まれています", name);
+        }
+
+        #endregion Throw
+    }
+}
