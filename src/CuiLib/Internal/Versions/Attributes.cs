cd382a20abc2eb9aba36836bde6e0cb90dabe5b8
Funny_Silkie <funnysilkie@gmail.com>
2024-06-24 17:56:32 +0900

    refactor: add CallerArgumentExpressionAttribute

---
+++ src/CuiLib/Internal/Versions/Attributes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace System.Runtime.CompilerServices
+{
+#if NETSTANDARD
+
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    internal sealed class CallerArgumentExpressionAttribute : Attribute
+    {
+        public string ParameterName { get; }
+
+        public CallerArgumentExpressionAttribute(string parameterName)
+        {
+            ParameterName = parameterName;
+        }
+    }
+
+#endif
+}
