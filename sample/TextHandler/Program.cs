b131f16c8f4d2709730f2fd07540df449a1d0667
Funny_Silkie <funnysilkie@gmail.com>
2024-11-27 11:49:18 +0900

    chore: init sample app project

---
+++ sample/TextHandler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TextHandler
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
